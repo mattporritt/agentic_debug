1bb8522443f7b5c1bd7340e6a9e48d779941ea93
Matt Porritt <matt.porritt@moodle.com>
2026-04-17 09:04:06 +1000

    Apply Moodle Community License headers

--- src/moodle_debug/server/MoodleContextMapper.php
+++ src/moodle_debug/server/MoodleContextMapper.php
@@ -1,5 +1,10 @@
 <?php
 
+// Copyright (c) Moodle Pty Ltd. All rights reserved.
+// Licensed under the Moodle Community License v1.3.
+// See LICENSE.md in the repository root for full terms.
+// Commercial use requires a separate written agreement with Moodle.
+
 declare(strict_types=1);
 
 namespace MoodleDebug\server;
